163f2d1e94b9d283e892f2ce91ca7dfcb65a3719
Lucas Leitche <llneves019@gmail.com>
2022-07-20 11:28:57 -0300

    Front-end setups

--- web/src/components/Widget-form/ScreenshotButton.tsx
+++ web/src/components/Widget-form/ScreenshotButton.tsx
@@ -1,11 +1,11 @@
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import html2canvas from "html2canvas"
 import { useState } from "react";
 import { Loading } from "../Loading";
 
 interface ScreenshotButtonProps {
-    screenshot: string;
-    onScreenshotTook: (screenshot: string) => void; 
+    screenshot: string | null, // Para informar que a variavel pode receber um valor do tipo string ou valor pode ser nulo
+    onScreenshotTook: (screenshot: string | null) => void; 
 }
 
 export function ScreenshotButton({
@@ -15,34 +15,44 @@ export function ScreenshotButton({
     const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
 
     async function handleTakeScreenshot(){
-        
         setIsTakingScreenshot(true);
 
         const canvas = await html2canvas(document.querySelector('html')!);
         const base64image = canvas.toDataURL('image/png');
-        
+        console.log(base64image);
         onScreenshotTook(base64image);
 
         setIsTakingScreenshot(false);
     }
+
     if(screenshot){
         return(
-            <img src={screenshot}/>
+           <button
+            type="button"
+            className="p-1 w-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+            onClick={() => onScreenshotTook(null)}
+            style={{
+                backgroundImage: `url(${screenshot})`,
+                backgroundPosition: 'right bottom',
+                backgroundSize: 180
+            }}
+           >
+            <Trash  weight="fill"/>
+           </button>
         )
     }
-    else{
-        return(
-            <button
-                onClick={handleTakeScreenshot}
-                type="button"
-                className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-500 "
-            >
-                { 
-                    !isTakingScreenshot ? <Camera className="w-6 h-6 text-zinc-100"/> :<Loading/>
-    
-                }
-            </button>
-        )
+   
+    return(
+        <button
+            onClick={handleTakeScreenshot}
+            type="button"
+            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-500 "
+        >
+            { 
+                !isTakingScreenshot ? <Camera className="w-6 h-6 text-zinc-100"/> :<Loading/>
 
-    }
+            }
+        </button>
+    )
+
 }
--- web/src/components/Widget-form/index.tsx
+++ web/src/components/Widget-form/index.tsx
@@ -7,6 +7,8 @@ import ideaImageUrl from '../../assets/Idea.svg';
 import thoughtImageUrl from '../../assets/Thought.svg';
 import { FeedBackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedBackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccess } from "./Steps/FeedbackSuccessStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccesSetp";
 export const feedbackTypes = {
     BUG:{
         title:'Problema',
@@ -37,25 +39,37 @@ export function WidgetForm (){
     
     const [feedbackType, setFeedbackType] = useState<FeedBackType | null>(null) // Primeiro parametro do array é a variavel de estado a segunda e função que seta o estado
     
+    const [feedBackSent, setFeedbackSent] = useState(false);
+
+
     function handleRestartFeedback(){
         setFeedbackType(null)
+
+        setFeedbackSent(false)
     }
+
     
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            
-
             {
-                !feedbackType ? (
-                    <FeedBackTypeStep onFeedBackTypeChanged={setFeedbackType}/>
-                ): (
-                    <FeedBackContentStep 
-                        onFeedbackRestartRequested={handleRestartFeedback} 
-                        feedbackType={feedbackType}
-                    />
+                feedBackSent ? (
+                    <FeedbackSuccessStep onFeedBackTypeChanged={ handleRestartFeedback}/>
+                ) : (
+                    <>
+                        {
+                            !feedbackType ? (
+                                <FeedBackTypeStep onFeedBackTypeChanged={setFeedbackType}/>
+                            ): (
+                                <FeedBackContentStep 
+                                    onFeedbackRestartRequested={handleRestartFeedback}
+                                    onFeedbackSent={()=> setFeedbackSent(true)}
+                                    feedbackType={feedbackType}
+                                />
+                            )
+                        }
+                    </>
                 )
-            }
-            
+            }    
             <footer className="text-center">
                 Feito com ♥ por <a href="#" className="underline-offset-2">Lucas Leite</a> <a href="https://rockeatseat.com.br"><br />(Projeto Rocketseat)</a> 
             </footer>
